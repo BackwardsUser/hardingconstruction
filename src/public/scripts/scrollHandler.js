ea623529b2e6dcaa199128b926ea72d3da8aefa2
Alexander Harding <AlexHardingYT@gmail.com>
2025-07-13 08:50:02 -0400

    Rearranged Website
    MOTD felt a little unnecessary, removed MOTD and redesigned Header to make less prominant

--- src/public/scripts/scrollHandler.js
+++ src/public/scripts/scrollHandler.js
@@ -1,14 +1,14 @@
-var motd = document.getElementById('motd');
+// var motd = document.getElementById('motd');
 var header = document.getElementById('header');
 var title = document.getElementById('title');
 document.addEventListener('scroll', e => {
     var scrollingElement = e.target.scrollingElement
     if (scrollingElement.scrollTop == 0) {
-        motd.classList.remove('hidden');
+        // motd.classList.remove('hidden');
         header.classList.remove('small');
         title.classList.remove('small-title');
     } else {
-        motd.classList.add('hidden');
+        // motd.classList.add('hidden');
         header.classList.add("small");
         title.classList.add("small-title");
     }
